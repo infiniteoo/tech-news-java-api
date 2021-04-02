db70d7b7cc4eb9243c1e3de5799cf5d6bffb7663
Troy Dorman <troydorman@gmail.com>
2021-04-02 08:12:09 -0600

    created the Comment entity model for the database

--- tech-news-java-api/src/main/java/com/technews/model/User.java
+++ tech-news-java-api/src/main/java/com/technews/model/User.java
@@ -3,6 +3,7 @@ package com.technews.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
